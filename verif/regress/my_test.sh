ecc52f24e17060c1b07623362631f5d696b2e872
HeshanDissanayake <tharindudissa18@gmail.com>
2025-01-30 12:28:11 +1100

    dir change

--- verif/regress/my_test.sh
+++ verif/regress/my_test.sh
@@ -2,7 +2,7 @@ export DV_OPTS="$DV_OPTS --issrun_opts=+debug_disable=1+UVM_VERBOSITY=UVM_NONE"
 CC_OPTS="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -I../tests/custom/env -I../tests/custom/common -lgcc"
 DV_SIMULATORS=veri-testharness
 
-export CVA6_REPO_DIR="/home/heshds/working_dir/cva6_new/cva6"
+export CVA6_REPO_DIR="/home/heshds/working_dir/cva6"
 export RISCV="/opt/dev/cva6_riscv_regsw"
 export TRACE_FAST=1
 
@@ -18,4 +18,5 @@ make -C ../.. clean
 make clean_all
 
 # python3 cva6.py --asm_tests /home/heshds/working_dir/cva6_new/cva6/verif/tests/custom/hello_world/custom_test_template.S --iss_yaml cva6.yaml --target cv64a6_imafdc_sv39 --iss=$DV_SIMULATORS --gcc_opts="$CC_OPTS -T ../tests/custom/common/test.ld" $DV_OPTS
-python3 cva6.py --asm_tests /home/heshds/working_dir/cva6_new/cva6/verif/tests/custom/hello_world/custom_test_template.S --iss_yaml cva6.yaml --target cv64a6_imafd_sv39 --iss=$DV_SIMULATORS --gcc_opts="$CC_OPTS -T ../tests/custom/common/test.ld" $DV_OPTS
+python3 cva6.py --asm_tests /home/heshds/working_dir/cva6/verif/tests/custom/hello_world/custom_test_template.S --iss_yaml cva6.yaml --target cv64a6_imafd_sv39 --iss=$DV_SIMULATORS --gcc_opts="$CC_OPTS -T ../tests/custom/common/test.ld" $DV_OPTS
+                            
